70d6fb602971da3d6e2ed59d2a0eb164e718f0cf
almar <almarjavier24@gmail.com>
2022-06-26 23:04:46 +0800

    Reverse an integer from leetcode problem

---
+++ Reverse an Integer/README.md
@@ -0,0 +1,1 @@
+This is my take on leetcode problem link: https://leetcode.com/problems/reverse-integer/submissions/
---
+++ Reverse an Integer/src/reverse/an/integer/ReverseAnInteger.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reverse.an.integer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Almar
+ */
+public class ReverseAnInteger {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        System.out.print(reversedInt(Integer.parseInt(input)));
+    }
+    static int reversedInt(int x){           
+        int r = 0;
+        long ans = 0;
+        while(x != 0){
+            r = x%10; //System.out.println(r);
+            ans = ans * 10 + r;// System.out.println(ans);
+            if( ans >= Integer.MAX_VALUE || ans <= Integer.MIN_VALUE){
+            }
+            x = x/10;
+            
+        }
+        return (int) ans;
+        }
+}
