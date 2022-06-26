aa0122f476cbe5ae91212606e65ac9aa1a3ad609
almar <almarjavier24@gmail.com>
2022-06-26 23:11:12 +0800

    Reverse an Integer problem from leetcode

--- Reverse an Integer/src/reverse/an/integer/ReverseAnInteger.java
+++ Reverse an Integer/src/reverse/an/integer/ReverseAnInteger.java
@@ -27,12 +27,12 @@ public class ReverseAnInteger {
         int r = 0;
         long ans = 0;
         while(x != 0){
-            r = x%10; //System.out.println(r);
-            ans = ans * 10 + r;// System.out.println(ans);
+            r = x%10; 
+            ans = ans * 10 + r;
             if( ans >= Integer.MAX_VALUE || ans <= Integer.MIN_VALUE){
+                return 0;
             }
             x = x/10;
-            
         }
         return (int) ans;
         }
