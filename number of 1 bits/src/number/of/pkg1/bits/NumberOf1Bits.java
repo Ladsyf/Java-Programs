df9a2c6d0403d6972df2a0bcd526adab327f9a62
almar <almarjavier24@gmail.com>
2022-07-05 21:03:08 +0800

    unsolved leet code problem

---
+++ number of 1 bits/src/number/of/pkg1/bits/NumberOf1Bits.java
@@ -0,0 +1,18 @@
+package number.of.pkg1.bits;
+
+public class NumberOf1Bits {
+
+    public static void main(String[] args) {
+        String bin = "00000000000000000000000010000000";
+        System.out.println(hammingweight(Integer.parseInt(bin)));
+    }
+    static int hammingweight(int n){
+    int ans = 0;
+        for(int i = 0; i < 31; i++){
+            
+        }
+        
+    return ans;
+    }
+    
+}
